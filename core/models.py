98e4fe2366d619d8077cb7d8322dae4c2f062cef
Carlos-Daniel-SS <carlossousa@rede.ulbra.br>
2023-12-22 18:04:50 -0300

    Adicionando mais uma url para ver data dos eventos

--- core/models.py
+++ core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 class Eventos(models.Model):
+
     titulo = models.CharField(verbose_name='Titulo do Evento', max_length=100)
     descricao = models.TextField(verbose_name='Descrição do Evento', blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
@@ -10,5 +13,7 @@ class Eventos(models.Model):
 
     class Meta:
         db_table = 'evento'
+
     def __str__(self):
-        return self.titulo
+        return (f'Titulo: {self.titulo}| Descrição: {self.descricao}| Data do evento: {self.data_evento}| Data de '
+                f'criação: {self.data_criacao}')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Eventos
+
 
 # Create your views here.
+
+def ver_evento(request, titulo_evento):
+    evento = Eventos.objects.get(titulo=titulo_evento)
+    return HttpResponse(f'<h1>Data do Evento: {evento.data_evento}</h1>')
