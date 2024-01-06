269f1ca7c7c16a7bf00256418dcb11049520e83f
Carlos Daniel <carlossousa@rede.ulbra.br>
2024-01-06 18:10:04 -0300

    Criando tela de login e autenticação

--- core/views.py
+++ core/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Eventos
-
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 # Create your views here.
+def login_user(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou Senha inválido !")
+
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    evento = Eventos.objects.filter(usuario=usuario)
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
 
 def ver_evento(request, titulo_evento):
     evento = Eventos.objects.get(titulo=titulo_evento)
     return HttpResponse(f'<h1>Data do Evento: {evento.data_evento}</h1>')
-
-def lista_eventos(request):
-    evento = Eventos.objects.all()
-    dados = {'eventos': evento}
-    return render(request, 'agenda.html', dados)
-
-#def index(request):
- #   return redirect('/agenda/')
