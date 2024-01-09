3e1743b4071e70026df86ee5a844c98550adca4a
Carlos-Daniel-SS <carlossousa@rede.ulbra.br>
2024-01-09 18:08:22 -0300

    Adicionando função de excluir evento

--- core/views.py
+++ core/views.py
@@ -48,3 +48,11 @@ def submit_evento(request):
         usuario = request.user
         Eventos.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
     return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Eventos.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
